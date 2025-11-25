05a9d2a4be5ee54ade6780b2afd6986eddb04968
Franco Carranza <francocarranza144@gmial.com>
2025-11-25 19:32:04 -0300

    creacion front-end

---
+++ front-end/src/components/EventCard/EventCard.jsx
@@ -0,0 +1,15 @@
+import './EventCard.css';
+
+function EventCard() {
+    return (
+        <div className="event-card">
+            <h2>Asadito despedida USA</h2>
+            <p>06/12/2025 21:00</p>
+            <p>Casa Carri</p>
+            <p>Matambre a la pizza</p>
+            <button>Ver mas</button>
+        </div>
+    );
+}
+
+export default EventCard;
---
+++ front-end/src/components/Inicio/Inicio.jsx
@@ -0,0 +1,12 @@
+import './Inicio.css';
+import EventCard from '../EventCard/EventCard';
+
+function Inicio() {
+    return (
+        <div>
+            <EventCard />
+        </div>
+    );
+}
+
+export default Inicio;
